1a90241c6b40961b1d7fb0d9996642ec49c371fa
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-08 21:24:28 -0400

    Added: refactored with lombok

--- src/main/java/com/planos/appbackend/apis/model/UserModel.java
+++ src/main/java/com/planos/appbackend/apis/model/UserModel.java
@@ -1,52 +1,17 @@
-package com.example.planosapi.apis.model;
+package com.planos.appbackend.apis.model;
 
+import com.planos.appbackend.enums.UserModelGender;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class UserModel {
-
-    private int id;
-    private String name;
-    private int age;
-    private String email;
-
-    public UserModel(int id, String name, int age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    //Getters
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    //Setters
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    @Id
+    private final int id;
+    private final String name;
+    private final UserModelGender gender;
+    private final int age;
+    private final String email;
 }
