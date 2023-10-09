724a5dbcd3a09d343f8ce4f700b0610c8c1a44a3
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-08 21:26:22 -0400

    Added: new test for api

--- src/main/java/com/planos/appbackend/service/UserService.java
+++ src/main/java/com/planos/appbackend/service/UserService.java
@@ -1,29 +1,27 @@
-package com.example.planosapi.service;
+package com.planos.appbackend.service;
 
-import com.example.planosapi.apis.model.UserModel;
+import com.planos.appbackend.apis.model.UserModel;
+import com.planos.appbackend.enums.UserModelGender;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 
 public class UserService {
 
-    private List<UserModel> userList;
+    private final List<UserModel> userList;
 
     public UserService() {
         userList = new ArrayList<>();
 
-        UserModel user1 = new UserModel(1, "Ida",  32,  "ida@mail.com");
-        UserModel user2 = new UserModel(2, "Hilda",  33,  "Hilda@mail.com");
+        UserModel user1 = new UserModel(1, "Ida", UserModelGender.Female,32,  "ida@mail.com");
+        UserModel user2 = new UserModel(2, "Hilda", UserModelGender.Female,33,  "Hilda@mail.com");
 
         userList.addAll(Arrays.asList(user1, user2));
     }
-    public Optional<UserModel> getUser(Integer id) {
-        Optional optional = Optional.empty();
+    public Optional<Object> getUser(Integer id) {
+        Optional<Object> optional = Optional.empty();
         for(UserModel user: userList){
             if(id == user.getId()){
                 optional = Optional.of(user);
