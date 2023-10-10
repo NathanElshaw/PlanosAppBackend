cf078d572acc51bc446aa6b9dd158978bfa6a164
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-09 22:35:25 -0400

    Added: constructor

--- src/main/java/com/planos/appbackend/apis/model/UserModel.java
+++ src/main/java/com/planos/appbackend/apis/model/UserModel.java
@@ -1,5 +1,6 @@
 package com.planos.appbackend.apis.model;
 
+import com.planos.appbackend.apis.model.subModels.UserModelCompany;
 import com.planos.appbackend.enums.UserModelGender;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
@@ -9,9 +10,31 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class UserModel {
     @Id
-    private final int id;
-    private final String name;
-    private final UserModelGender gender;
-    private final int age;
-    private final String email;
+    private  int id;
+    private  String name;
+    private  UserModelGender gender;
+    private  int age;
+    private  String email;
+    private  String username;
+    private  String password;
+    private  String verificationCode;
+    private  UserModelCompany companyInfo;
+
+    public UserModel(String name,
+                     UserModelGender gender,
+                     int age,
+                     String email,
+                     String username,
+                     String password,
+                     String verificationCode,
+                     UserModelCompany companyInfo) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.verificationCode = verificationCode;
+        this.companyInfo = companyInfo;
+    }
 }
