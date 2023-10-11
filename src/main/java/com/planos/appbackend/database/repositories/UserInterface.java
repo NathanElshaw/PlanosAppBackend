4bd753a6dc52bea08e4e1c82bed36a0ff493d88f
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-10 20:28:57 -0400

    Changed layout

--- src/main/java/com/planos/appbackend/database/repositories/UserInterface.java
+++ src/main/java/com/planos/appbackend/database/repositories/UserInterface.java
@@ -1,2 +1,8 @@
-package com.planos.appbackend.database.repositories;public interface UserInterface {
+package com.planos.appbackend.database.repositories;
+
+import com.planos.appbackend.apis.model.UserModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserInterface
+        extends MongoRepository<UserModel, String> {
 }
