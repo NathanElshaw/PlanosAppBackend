186851b8e8ebdb268df6cea5c3f197b833ea304b
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-11 22:00:35 -0400

    Added: date time to model

--- src/main/java/com/planos/appbackend/apis/model/subModels/UserModelCompany.java
+++ src/main/java/com/planos/appbackend/apis/model/subModels/UserModelCompany.java
@@ -4,11 +4,13 @@ package com.planos.appbackend.apis.model.subModels;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class UserModelCompany {
     private String company;
     private String companyId;
     private String companyRole;
-    private String dateJoined;
+    private LocalDateTime dateJoined;
 }
