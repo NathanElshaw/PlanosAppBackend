2516727c588b4bdcd18a30b41af9da1d835c4ca7
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-12 21:26:26 -0400

    Added: query

--- src/main/java/com/planos/appbackend/database/repositories/UserInterface.java
+++ src/main/java/com/planos/appbackend/database/repositories/UserInterface.java
@@ -1,8 +1,12 @@
 package com.planos.appbackend.database.repositories;
 
 import com.planos.appbackend.apis.model.UserModel;
+import org.apache.catalina.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
 
 public interface UserInterface
         extends MongoRepository<UserModel, String> {
+    Optional<UserModel> findUserModelByEmail(String email);
 }
