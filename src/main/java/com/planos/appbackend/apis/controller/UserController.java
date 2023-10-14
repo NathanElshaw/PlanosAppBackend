8e446aa6cb6873a9a1e5188902a18c7b233e7f85
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-13 21:17:17 -0400

    Added: request body

--- src/main/java/com/planos/appbackend/apis/controller/UserController.java
+++ src/main/java/com/planos/appbackend/apis/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.planos.appbackend.apis.controller;
 
 import com.planos.appbackend.apis.model.UserModel;
+import com.planos.appbackend.apis.model.validation.UserModelValidation;
 import com.planos.appbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -21,11 +22,17 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public UserModel getUser(@RequestParam String id) {
+    public UserModel getUser( @RequestParam String id) {
         Optional user = userService.getUser(id);
         if(user.isPresent()){
             return (UserModel) user.get();
         }
     return null;
+    }
+
+    @PostMapping("/user")
+    public UserModel addUser(@RequestBody UserModel payload){
+        UserModelValidation
+        return null;
     }
 }
